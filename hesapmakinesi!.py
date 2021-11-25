598f9a87a973c04fd05f253825a2e9206e8e98f9
mefors <94995745+mefors@users.noreply.github.com>
2021-11-25 19:14:03 +0300

    Add files via upload

---
+++ hesapmakinesi!.py
@@ -0,0 +1,27 @@
+print("sonucu 31 çıkıcak işlemler yapmayınız!(makine heyecanlanıyo)")
+islem = input("işlem giriniz (+ , - , / , *): ")
+a = int(input("1.sayı: "))
+b =int(input("2.sayı: "))
+if islem == "+" :
+    if a+b == 31:
+        print("sadjcaklsdkalcdas")
+    else:
+        print(a+b)
+if islem == "-" :
+    if a-b == 31:
+        print("sadjcaklsdkalcdas")
+    else:
+        print(a-b)
+if islem == "/" :
+    if a/b == 31:
+        print("sadjcaklsdkalcdas")
+    else:
+        print(a/b)
+if islem == "*" :
+    if a*b == 31:
+        print("sadjcaklsdkalcdas")
+    else:
+        print(a*b)
+input("ENTER tuşuna basarak çıkış yapabilirsiniz...")
+
+
